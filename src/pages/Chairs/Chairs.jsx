8d09ab0b27b893d8913a4f34987ac313b7856f7a
JBalls <jballs@Viktorias-iMac.attlocal.net>
2024-02-24 13:55:21 -0600

    working on chairs

--- src/pages/Chairs/Chairs.jsx
+++ src/pages/Chairs/Chairs.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export const Chairs = () => {
   return (
-    <div>Chairs</div>
+    <div className='pt-12'>
+    
+    <h1 className='text-center text-indigo-800 font-bold text-4xl pt-2'>Shop our ergonomic desks collection</h1>
+    <div className=" products lg:flex justify-between mr-10 ml-10 overflow-x-auto">
+        {""}
+        {PRODUCTS.slice(4).map((product) => (
+          <Product data={product} />
+        ))}
+
+      </div>
+    
+    </div>
   )
 }
